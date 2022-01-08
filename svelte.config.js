ce7c509ef3deccafd5e6580aa7a8eef1100fc35f
Martin_Leshi <martinleshi@hotmail.com>
2022-01-08 12:24:28 +0100

    updated-to-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 //import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
